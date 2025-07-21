e13ae52cde36c11cf679cf08e2641a6006b69611
wkdus0608 <seongmin020608@gmail.com>
2025-07-21 23:00:40 +0900

    feat: Add drag and drop functionality for todos

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import AppNavigator from './src/navigation/AppNavigator';
 
 function App() {
-  return <AppNavigator />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AppNavigator />
+    </GestureHandlerRootView>
+  );
 }
 
 export default App;
